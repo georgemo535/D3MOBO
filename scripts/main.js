7368eec44dc3189881e000b9aa4baf168c6d9066
George <georgemo535@github.com>
2022-05-17 14:34:08 +0100

    finished basic layout

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,17 @@ const svgPcp = d3.select("#pcp")
 .append("g")
   .attr("transform",
         `translate(${margin.left},${margin.top})`);
+      
+const tooltipPcp = d3.select("#pcp")
+.append("div")
+.style("opacity", 1)
+.attr("class", "tooltip")
+.style("background-color", "white")
+.style("border", "solid")
+.style("border-width", "1px")
+.style("border-radius", "5px")
+.style("padding", "10px")
+    
 
 // Parse the Data
 d3.csv("../data/data.csv").then( function(data) {
@@ -55,6 +66,7 @@ d3.csv("../data/data.csv").then( function(data) {
       .style("stroke-width", "4")
     
       let idVal = d3.select(this).attr("value");
+
       svgScatter.selectAll(".dot").
       filter(function() {
           return d3.select(this).attr("value") == idVal
@@ -62,19 +74,41 @@ d3.csv("../data/data.csv").then( function(data) {
         .duration(200)
         .style("fill", "green")
         .attr("r", 7)
+
+    tooltipPcp.html("Design parameters: " + String(d3.select(this).attr("design")))
+      .transition()
+      .duration(500)
+      .style("opacity", 1)
+      .style("left", (event.x) / 2 + "px")
+      .style("top", (event.y) / 2 + "px")
+      
+    let objectivesDesign = svgScatter.selectAll(".dot").
+    filter(function(){
+      return d3.select(this).attr("value") == idVal;
+    }).attr("objectives");
+
+    console.log(objectivesDesign);
+
+    tooltipScatter.html("Objectives: " + String(objectivesDesign))
+    .transition()
+    .duration(500)
+    .style("opacity", 1)
+    .style("left", (event.x) / 2 + "px")
+    .style("top", (event.y) / 2 + "px")
+
   }
 
   const doNotHighlightPcp = function(event, d){
     d3.select("#pcp").selectAll(".line")
-      .transition().duration(200).delay(500)
+      .transition().duration(200).delay(100)
       .style("stroke", function(d){ return("lightblue")} )
       .style("opacity", "1")
       .style("stroke-width", "2")
     
     d3.select("#scatter").selectAll(".dot")
       .transition()
-      .duration(200).delay(500)
-      .style("fill", "green")
+      .duration(200).delay(100)
+      .style("fill", "lightblue")
       .attr("r", 5 )
   }
 
@@ -129,6 +163,16 @@ const svgScatter = d3.select("#scatter")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
+const tooltipScatter = d3.select("#scatter")
+.append("div")
+.style("opacity", 1)
+.attr("class", "tooltip")
+.style("background-color", "white")
+.style("border", "solid")
+.style("border-width", "1px")
+.style("border-radius", "5px")
+.style("padding", "10px")
+
 d3.csv("../data/data.csv").then( function(data) {
     // Add X axis
     const x = d3.scaleLinear()
@@ -145,7 +189,7 @@ d3.csv("../data/data.csv").then( function(data) {
     svgScatter.append("g")
         .call(d3.axisLeft(y));
 
-    const highlightScatter = function(d){
+    const highlightScatter = function(event, d){
       // console.log(svgScatter.selectAll(".dot"));
       d3.select("#scatter").selectAll(".dot")
         .transition()
@@ -163,8 +207,16 @@ d3.csv("../data/data.csv").then( function(data) {
         .duration(200)
         .style("fill", "green")
         .attr("r", 7)
+      
+      tooltipScatter.html("Objectives: " + d3.select(this).attr("objectives"))
+        .transition()
+        .duration(500)
+        .style("opacity", 1)
+        .style("left", (event.x) / 2 + "px")
+        .style("top", (event.y) / 2 + "px")
 
       let idVal = d3.select(this).attr("value");
+
       svgPcp.selectAll(".line").
       filter(function() {
           return d3.select(this).attr("value") == idVal
@@ -172,16 +224,28 @@ d3.csv("../data/data.csv").then( function(data) {
         .style("stroke", "green")
         .style("opacity", "1")
         .style("stroke-width", "4");
+
+      let parametersDesign = svgPcp.selectAll(".line").
+      filter(function(){
+        return d3.select(this).attr("value") == idVal;
+      }).attr("design");
+
+      tooltipPcp.html("Design Parameters: " + parametersDesign)
+        .transition()
+        .duration(500)
+        .style("opacity", 1)
+        .style("left", (event.x) / 2 + "px")
+        .style("top", (event.y) / 2 + "px")
     }
         
-    const doNotHighlightScatter = function(){
-        d3.select("#scatter").selectAll(".dot")
-            .transition()
-            .duration(200).delay(500)
-            .style("fill", "green")
-            .attr("r", 5 )
-        
-        d3.select("#pcp").selectAll(".line")
+    const doNotHighlightScatter = function(event, d){
+      d3.select("#scatter").selectAll(".dot")
+        .transition()
+        .duration(200).delay(500)
+        .style("fill", "lightblue")
+        .attr("r", 5 )
+      
+      d3.select("#pcp").selectAll(".line")
         .transition().duration(200).delay(500)
         .style("stroke", function(d){ return("lightblue")} )
         .style("opacity", "1")
@@ -196,36 +260,42 @@ d3.csv("../data/data.csv").then( function(data) {
     .append("circle")
         .attr("value", function (d) {return d.id;})
         .attr("class", function (d) { return "dot" } )
+        .attr("objectives", function (d) {return [d.y1, d.y2]; })
         .attr("cx", function (d) { return x(d.y1); } )
         .attr("cy", function (d) { return y(d.y2); } )
         .attr("r", 5)
-        .style("fill", "green")
+        .style("fill", "lightblue")
     .on("mouseover", highlightScatter)
     .on("mouseleave", doNotHighlightScatter)
 })
 
 const svgRegions = d3.select("#regions")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+  "translate(" + margin.left + "," + margin.top + ")");
+  
+const tooltipRegions = d3.select("#regions")
+  .append("div")
+  .style("opacity", 1)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "1px")
+  .style("border-radius", "5px")
+  .style("padding", "10px")
 
 d3.csv("../data/regions.csv").then( function(data) {
 
   // Here I set the list of dimension manually to control the order of axis:
   dimensions = ["x1", "x2", "x3", "x4", "x5"]
-  dimensionsLower = ["x1low", "x2low", "x3low", "x4low", "x5low"]
-  dimensionsUpper = ["x1up", "x2up", "x3up", "x4up", "x5up"]
-
-  dimLowerConvert = {"x1low": "x1", "x2low": "x2", "x3low": "x3", "x4low": "x4", "x5low": "x5"};
-  dimUpperConvert = {"x1up": "x1", "x2up": "x2", "x3up": "x3", "x4up": "x4", "x5up": "x5"};
   
   // For each dimension, I build a linear scale. I store all in a y object
   const yPcp = {}
-  for (i in dimensionsPcp) {
-    name = dimensionsPcp[i]
+  for (i in dimensions) {
+    name = dimensions[i]
     yPcp[name] = d3.scaleLinear()
       .domain( [0,1] ) // --> Same axis range for each group
       // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
@@ -239,24 +309,49 @@ d3.csv("../data/regions.csv").then( function(data) {
 
   // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
   function pathRegion(d) {
-    console.log(d);
-    console.log(dimensions.map(function(p) {return x(p); }));
-    console.log(dimensionsLower.map(function(p) {return yPcp[dimLowerConvert[p]](d[p]); }));
-    console.log(dimensionsUpper.map(function(p) {return yPcp[dimUpperConvert[p]](d[p]); }));
-    // console.log(d);
-    // const area = d3.area().x(dimensions.map(function(p) {return x(p); }))
-    //       .y0(dimensionsLower.map(function(p) {return yPcp[dimLowerConvert[p]](d[p]); }))
-    //       .y1(dimensionsUpper.map(function(p) {return yPcp[dimUpperConvert[p]](d[p]); }));
-    // console.log(area);
-    d3.area().x(dimensions.map(function(p) {return x(p); }))
-            .y0(dimensionsLower.map(function(p) {return yPcp[dimLowerConvert[p]](d[p]); }))
-            .y1(dimensionsUpper.map(function(p) {return yPcp[dimUpperConvert[p]](d[p]); }));
-    // d3.area([[0, 368, 322], [100, 322, 276], [200, 368, 322], [300, 322, 276], [400, 368, 322]]);
-    // d3.area().x(d => x(0)) .y0(d=>y(d.x1low)) .y1(d => y(d.x1up)); 
-    // d3.area().x(d => x(100)) .y0(d=>y(d.x2low)) .y1(d => y(d.x2up)); 
-    // d3.area().x(d => x(200)) .y0(d=>y(d.x3low)) .y1(d => y(d.x3up)); 
-    // d3.area().x(d => x(300)) .y0(d=>y(d.x4low)) .y1(d => y(d.x4up)); 
-    // d3.area().x(d => x(400)) .y0(d=>y(d.x5low)) .y1(d => y(d.x5up)); 
+    var regionPath = "M ";
+    regionPath += 0 + " " + (1 - d.x1low)*height + " L ";
+    regionPath += 100 + " " + (1 - d.x2low)*height + " L ";
+    regionPath += 200 + " " + (1 - d.x3low)*height + " L ";
+    regionPath += 300 + " " + (1 - d.x4low)*height + " L ";
+    regionPath += 400 + " " + (1 - d.x5low)*height + " L ";
+    regionPath += 400 + " " + (1 - d.x5up)*height + " L ";
+    regionPath += 300 + " " + (1 - d.x4up)*height + " L ";
+    regionPath += 200 + " " + (1 - d.x3up)*height + " L ";
+    regionPath += 100 + " " + (1 - d.x2up)*height + " L ";
+    regionPath += 0 + " " + (1 - d.x1up)*height;
+    regionPath += " Z";
+    return regionPath;
+  }
+
+  const highlightArea = function(event, d){
+    // console.log(svgScatter.selectAll(".dot"));
+    d3.select("#regions").selectAll(".area")
+      .transition()
+      .duration(200)
+      .style("fill", "lightgrey")
+      .style("opacity", 0.5)
+
+    d3.select(this)
+      .transition()
+      .duration(200)
+      .style("fill", "blue")
+      .style("opacity", d3.select(this).attr("confidence") * 0.7 + 0.3)
+    
+    tooltipRegions.html("ID: " + d3.select(this).attr("value"))
+      .transition()
+      .duration(500)
+      .style("opacity", 1)
+      .style("left", (event.x) / 2 + "px")
+      .style("top", (event.y) / 2 + "px")
+  }
+
+  const doNotHighlightArea = function(event, d){
+    d3.select("#regions").selectAll(".area")
+      .transition()
+      .duration(200)
+      .style("fill", "lightblue")
+      .style("opacity", d => d.confidence * 0.7 + 0.3)
   }
 
   // Draw the areas
@@ -265,18 +360,15 @@ d3.csv("../data/regions.csv").then( function(data) {
   .data(data).enter()
   .append("path")
     .attr("class", "area")
-    .attr("d", pathRegion)
-    // .attr("d", function(d) {return pathRegion(d);} )
-    // .attr("d","M0,0L0.2,0L0.4,100L0.3,100L0.4,100Z")
-    // .attr("d", d3.area().x(dimensions.map(function(p) {return x(p); }))
-    // .y0(dimensionsLower.map(function(p) {return yPcp[dimLowerConvert[p]](d[p]); }))
-    // .y1(dimensionsUpper.map(function(p) {return yPcp[dimUpperConvert[p]](d[p]); })))
-    // .attr("d", d3.area([[0, 368, 322], [100, 322, 276], [200, 368, 322], [300, 322, 276], [400, 368, 322]]))
+    .attr("d", d => pathRegion(d))
+    .attr("upper", function(d) {return [d.x1low, d.x2low, d.x3low, d.x4low, d.x5low]})
+    .attr("lower", function(d) {return [d.x1up, d.x2up, d.x3up, d.x4up, d.x5up]})
     .attr("value", function (d) {return d.id; })
-    .style("fill", "blue" )
-    .style("opacity", 0.5)
-      // .on("mouseover", highlightPcp)
-      // .on("mouseout", doNotHighlightPcp)
+    .attr("confidence", function (d) {return d.confidence; })
+    .style("fill", "lightblue" )
+    .style("opacity", d => d.confidence * 0.7 + 0.3)
+  .on("mouseover", highlightArea)
+  .on("mouseout", doNotHighlightArea)
 
   // Draw the axis:
   svgRegions.selectAll("myAxis")
@@ -292,4 +384,19 @@ d3.csv("../data/regions.csv").then( function(data) {
       .style("fill", "black")
 
 })
-        
+
+var slider = document.getElementById("confidenceslider");
+var output = document.getElementById("confidenceslidervalue");
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+// var forbiddenRegionList = document.getElementById("regionslist")
+
+// function updateList() {
+//   var regionsData = $.csv.toObjects("../data/regions.csv");
+//   console.log(regionsData);
+// }
+  
+// updateList();
