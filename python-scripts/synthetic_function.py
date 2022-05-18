622cb37acb4f8d30e0e16f86a779ad7b19301c17
George <georgemo535@github.com>
2022-05-18 16:00:37 +0100

    added python scripts, did slider update interactivity

---
+++ python-scripts/synthetic_function.py
@@ -0,0 +1,19 @@
+from import_all import *
+
+def single_convex(x, weights, center, bias):
+    value = -np.dot(weights, (x - center) ** 2) + bias
+    return value
+
+def objective_function(X, weights, centers, biases, noise=0.0):
+    final = []
+    for x in X:
+        first = np.maximum(-1, single_convex(x, weights[0], centers[0], biases[0]) + 0.2 * np.random.uniform(low=-1.0, high=1.0))
+        second = np.maximum(-1, single_convex(x, weights[1], centers[1], biases[1]) + 0.2 * np.random.uniform(low=-1.0, high=1.0))
+        final.append([first, second])
+    return torch.tensor(final, dtype=torch.float64)
+
+def pilot_test_function(X, weights, centers, biases, noise=0.4):
+    return objective_function(X, weights, centers, biases, noise=noise)
+
+def formal_evaluation_function(X, weights, centers, biases, noise=0.1):
+    return objective_function(X, weights, centers, biases, noise=noise)
---
+++ scripts/sliders.js
@@ -0,0 +1,41 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+  }
+
+var numParams;
+var xBounds;
+
+function renderTaskInterface() {
+    readTextFile("../configs/config.json", function(text){
+        config = JSON.parse(text);
+        numParams = config["dimx"];
+        xBounds = config["xbounds"];
+
+        console.log(xBounds);
+        constructParameterSlider();
+    });
+}
+
+function constructParameterSlider() {
+    console.log("Loading sliders")
+
+    for (var i = 0; i < numParams; i++){
+        var inputTxt = "<input type='range' min=" + xBounds[i][0] + " max=" + xBounds[i][1] + " value='0.5' step='0.01' class='slider'"
+        + " id=" + "'param" + (i+1) + "slider'" + " name=" + "'param" + (i+1) + "'" + " oninput='this.nextElementSibling.value = this.value'>";
+        var outputTxt = "<output id='param"+ (i+1) + "output'>0.5</output>"
+        var breakTxt = "<br><br>"
+
+        // console.log(inputTxt);
+        $("#param-sliders").append(inputTxt)
+        $("#param-sliders").append(outputTxt)
+        $("#param-sliders").append(breakTxt)
+    }
+}
